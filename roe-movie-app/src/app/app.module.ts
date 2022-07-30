2a395df5ac88469392c4d76a6756bdce3069a759
OsamahNeamah <90590589+OsamahNeamah@users.noreply.github.com>
2022-07-30 13:48:30 -0400

    Add files via upload

--- roe-movie-app/src/app/app.module.ts
+++ roe-movie-app/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../header/header.component';
+import { HeaderTopComponent } from '../header/header-top/header-top.component';
+import { HeaderBannerComponent } from '../header/header-banner/header-banner.component'
+import { HeaderBannerContentComponent } from '../header/header-banner-content/header-banner-content.component';
+import { FooterComponent } from '../footer/footer.component';
+import { MainComponent } from '../main/main.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent,
+                  HeaderComponent, HeaderTopComponent,
+                  HeaderBannerComponent, HeaderBannerContentComponent, FooterComponent, MainComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ roe-movie-app/src/header/header-banner-content/header-banner-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-banner-content',
+  templateUrl: './header-banner-content.component.html',
+  styleUrls: ['./header-banner-content.component.css']
+})
+export class HeaderBannerContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
