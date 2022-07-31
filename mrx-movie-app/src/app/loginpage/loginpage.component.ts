558c2f0d80fb0010aec6868c0ef2369cdb7c395d
Meynard Duran <pvt_mduran18@yahoo.com>
2022-07-31 08:45:31 -0700

    added simple validations for input, biding input data

--- mrx-movie-app/src/app/loginpage/loginpage.component.ts
+++ mrx-movie-app/src/app/loginpage/loginpage.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loginpage.component.css'],
 })
 export class LoginpageComponent implements OnInit {
-  emailErrorMessage: string = 'Please enter a valid email or phone number.';
-  passwordErrorMessage: string =
-    'Your password must contain between 4 and 60 characters.';
+  emailErrorMessage: string = '';
+  passwordErrorMessage: string = '';
+  email: string = '';
+  password: string = '';
+  isChecked: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  validateEmail() {
+    const email = this.email;
+    if (email === '') {
+      this.emailErrorMessage = 'Please enter a valid email or phone number.';
+    }
+  }
+
+  validatePassword() {
+    const password = this.password;
+    if (password === '') {
+      this.passwordErrorMessage =
+        'Your password must contain between 4 and 60 characters.';
+    }
+  }
+
+  onChange() {
+    if (this.isChecked) {
+      console.log('remember!');
+    } else {
+      console.log('nothing');
+    }
+  }
+
+  submitForm() {
+    this.validateEmail();
+    this.validatePassword();
+    this.onChange();
+  }
 }
