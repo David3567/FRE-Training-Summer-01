563f143b42f49526e9eb507efea03e6eb35b5b11
lyuxu99 <lyuxu99@outlook.com>
2022-07-31 14:11:41 -0400

    added routing and some homepage contents

--- mrx-movie-app/src/app/app-routing.module.ts
+++ mrx-movie-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'login', component: LoginpageComponent},
+  {path: 'register', component: RegisterpageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
