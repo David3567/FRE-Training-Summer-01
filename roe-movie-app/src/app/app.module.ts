b42f16cb652ea30db0b76a14f1083959c895201b
ed-llc <edgar.munoz@louloucup.com>
2022-07-30 19:38:45 -0700

    Add register component

--- roe-movie-app/src/app/app.module.ts
+++ roe-movie-app/src/app/app.module.ts
@@ -8,13 +8,14 @@ import { HeaderTopComponent } from '../header/header-top/header-top.component';
 import { HeaderBannerComponent } from '../header/header-banner/header-banner.component'
 import { HeaderBannerContentComponent } from '../header/header-banner-content/header-banner-content.component';
 import { FooterComponent } from '../footer/footer.component';
-import { MainComponent } from '../main/main.component'
+import { MainComponent } from '../main/main.component';
+import { RegisterComponent } from '../register/register.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent,
                   HeaderComponent, HeaderTopComponent,
-                  HeaderBannerComponent, HeaderBannerContentComponent, FooterComponent, MainComponent ],
+                  HeaderBannerComponent, HeaderBannerContentComponent, FooterComponent, MainComponent, RegisterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ roe-movie-app/src/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  counter : number = 0;
+  email : string = '';
+  password : string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  incrementCount() {
+    this.counter++
+    console.log(this.counter)
+  }
+
+  setEmail(event : any) {
+    this.email = event.target.value;
+  }
+
+  setPassword(event : any) {
+    this.password = event.target.value;
+  }
+
+}
