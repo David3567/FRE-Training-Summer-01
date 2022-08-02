34a7c8939236d5f30bd194992107fd73ba69db2e
Rana0505 <73511883+piaoxianer@users.noreply.github.com>
2022-08-02 13:02:14 -0400

    img path changed, bg img container modified

--- movie-app/src/app/homepage/homepage.component.ts
+++ movie-app/src/app/homepage/homepage.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  homeBgimagePath = "assets/homepage_background_img.jpg"
+
   constructor() { }
 
   ngOnInit(): void {
