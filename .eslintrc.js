22ea23dc03e26e53661a2107a20195e07e80512c
Abel A <abelabrahamx@gmail.com>
2022-08-03 22:08:14 -0500

    eslint & .vscode setting add

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-useless-constructor': 0,
+  },
+};
