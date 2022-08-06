5cf8f6c42815e4a86bc7e34bb8e211509c01a374
Johnathan238 <jonathanumana86@gmail.com>
2022-08-06 18:26:59 -0400

    Got homepage almost, got movie-list componenet & got navbar but also sidebar nav component

--- Movie-App/src/app/app.module.ts
+++ Movie-App/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    MovieListComponent,
+    
   ],
   imports: [
     BrowserModule
