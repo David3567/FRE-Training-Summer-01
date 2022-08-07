f3c866afc19dd19ed8d459d4ffffc7d6354166e1
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2022-08-07 13:24:30 -0500

    removed conflicting routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// eslint-disable-next-line import/no-unresolved
-import { HomepageComponent } from './homepage/homepage.component';
-// eslint-disable-next-line import/no-unresolved
-import { LoginPageComponent } from './login-page/login-page.component';
-// eslint-disable-next-line import/no-unresolved
-import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomepageComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'login',
-    component: LoginPageComponent,
-  },
-];
+const routes: Routes = [{}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
