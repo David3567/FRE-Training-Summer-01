2d9f3513e56591a727e56f8b9a5d36433a099116
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2022-08-07 11:23:04 -0500

    login page as one page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// eslint-disable-next-line import/no-unresolved
+import { HomepageComponent } from './homepage/homepage.component';
+// eslint-disable-next-line import/no-unresolved
+import { LoginPageComponent } from './login-page/login-page.component';
+// eslint-disable-next-line import/no-unresolved
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/login-page/login-page.component.spec.ts
+++ src/app/login-page/login-page.component.spec.ts
@@ -1,23 +1,29 @@
+// eslint-disable-next-line import/no-unresolved
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// eslint-disable-next-line import/no-unresolved
 import { LoginPageComponent } from './login-page.component';
 
+// eslint-disable-next-line no-undef
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
+  // eslint-disable-next-line no-undef
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
+      declarations: [LoginPageComponent],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  // eslint-disable-next-line no-undef
   it('should create', () => {
+    // eslint-disable-next-line no-undef
     expect(component).toBeTruthy();
   });
 });
