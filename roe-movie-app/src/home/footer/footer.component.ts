6dbef342a657470c5f814b34ed73cc3beaa3fb86
Osamah <oneamah@gmail.com>
2022-08-06 20:41:26 -0400

    Some update on style

--- roe-movie-app/src/home/footer/footer.component.ts
+++ roe-movie-app/src/home/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  @Input() isHome:Boolean = false;
   constructor() { }
 
   ngOnInit(): void {
--- roe-movie-app/src/home/home.component.ts
+++ roe-movie-app/src/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MoviesService } from 'src/services/movies.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public Movies?:any = {}
+  constructor(private service:MoviesService) { }
 
   ngOnInit(): void {
+    this.service.getMovies().subscribe((result: any)=>{this.Movies=result; console.log(this.Movies)})
   }
 
 }
