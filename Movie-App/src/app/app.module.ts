9d7e27ef95f0797862c485c765ce3d002741c1d1
HawkEye Reeves <gregreeves280@gmail.com>
2022-08-08 15:07:01 -0400

    feature/S2A-10/register-page - Register page

--- Movie-App/src/app/app.module.ts
+++ Movie-App/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
 import { LoginComponent } from './components/login/login.component';
 
 import { MovieService } from './services/movie.service';
+import { RegisterComponent } from './components/register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     NavbarComponent,
     MovieListComponent,
-    LoginComponent, 
+    LoginComponent,
+    RegisterComponent, 
   ],
   imports: [BrowserModule, HttpClientModule],
   providers: [MovieService],
