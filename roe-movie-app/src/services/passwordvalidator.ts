d3e0c92b70f1471293c2e68e4d70727dacb85eef
ed-llc <edgar.munoz@louloucup.com>
2022-08-07 18:27:07 -0700

    Add custom password validators

---
+++ roe-movie-app/src/services/passwordvalidator.ts
@@ -0,0 +1,35 @@
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
+
+export function createPasswordStrengthValidator(): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+
+        const value = control.value;
+        
+        if (!value) {
+            return null;
+        }
+
+        const hasUpperCase = /[A-Z]+/.test(value);
+
+        const hasLowerCase = /[a-z]+/.test(value);
+
+        const hasNumeric = /[0-9]+/.test(value);
+
+        const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+        return !passwordValid ? {passwordStrength:true}: null;
+    }
+}
+
+export function createPasswordMatchValidator(fg: any): ValidatorFn {
+    return (control:AbstractControl) : ValidationErrors | null => {
+
+        const value = control.value;
+
+        if (!value) {
+            return null;
+        }
+
+        return value === fg.person.password ? null : {passwordMatch: true}
+    }
+}
