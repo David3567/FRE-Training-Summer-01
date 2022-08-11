109ad2deae084183d1ced417c08b52e4c34f76b3
bjanvier <bwemerezagabe@yahoo.fr>
2022-08-10 23:26:08 -0400

    Added lazy-loading for three main pages

--- movie-app/src/app/movies-list/movies-list/movies-list.module.ts
+++ movie-app/src/app/movies-list/movies-list/movies-list.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MoviesListComponent } from '../movies-list.component';
 import { MovieCardComponent } from '../../movie-card/movie-card.component';
 import { ShortPipe } from '../../pipes/short.pipe';
-import { BrowserModule } from '@angular/platform-browser';
-import { MoviesLisRoutingModule } from './movies-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     FontAwesomeModule,
     CommonModule
-    // MoviesLisRoutingModule,
-    // BrowserModule,
   ],
 })
 export class MoviesListModule { }
