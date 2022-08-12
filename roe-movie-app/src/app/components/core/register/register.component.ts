fba81e524f9f502d283527f3d6ed079d06331fb2
ed-llc <edgar.munoz@louloucup.com>
2022-08-11 19:01:32 -0700

    Moving current services to register component

--- roe-movie-app/src/app/components/core/register/register.component.ts
+++ roe-movie-app/src/app/components/core/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { pswduppercase, pswdnumeric, pswdlowercase, passwordMatch } from 'src/services/passwordvalidator';
+import { pswduppercase, pswdnumeric, pswdlowercase, passwordMatch } from '../../../services/passwordvalidator';
 
 
 @Component({
