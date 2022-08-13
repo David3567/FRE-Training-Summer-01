18456aff66319eb8c6bfed02cc76e4788855296e
Yue Dong <ydong011@fiu.edu>
2022-08-12 22:19:35 -0500

    update

--- Angular/performance/src/app/app-routing.module.ts
+++ Angular/performance/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LazyComponent } from './lazy/lazy.component';
+import { HelloComponent } from './hello/hello.component';
+import { EagerComponent } from './eager/eager.component';
 
-const routes: Routes = [{ path: 'lazy', component: LazyComponent }];
+const routes: Routes = [
+  { path: 'hello', component: HelloComponent },
+  { path: 'eager', component: EagerComponent },
+  {
+    path: 'lazy',
+    loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'hello' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
