ae14ab0112529ccc87f1fc0cbed96bac7fc8f340
Johnathan <jonathanumana86@gmail.com>
2022-08-13 18:36:52 -0400

    Got route working, bit funky

--- Movie-App/src/app/app.module.ts
+++ Movie-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
@@ -18,7 +19,12 @@ import { MovieService } from './services/movie.service';
     LoginComponent,
     RegisterComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path:'login', component: LoginComponent },
+    ]),],
   providers: [MovieService],
   bootstrap: [AppComponent],
 })
--- Movie-App/src/app/components/navbar/navbar.component.ts
+++ Movie-App/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   collapsed = true;
+
+
   constructor() { }
 
   ngOnInit(): void {
