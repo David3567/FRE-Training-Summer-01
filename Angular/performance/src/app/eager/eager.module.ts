d30666b9fd2f37232209080a966ebed191fa8ec8
Yue Dong <ydong011@fiu.edu>
2022-08-12 22:30:50 -0500

    update

--- Angular/performance/src/app/eager/eager.module.ts
+++ Angular/performance/src/app/eager/eager.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EagerComponent } from './eager.component';
-
-
+import { EagerService } from './eager.service';
 
 @NgModule({
-  declarations: [
-    EagerComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [EagerComponent],
+  imports: [CommonModule],
+  providers: [EagerService],
 })
-export class EagerModule { }
+export class EagerModule {}
--- Angular/performance/src/app/lazy/lazy.module.ts
+++ Angular/performance/src/app/lazy/lazy.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LazyComponent } from './lazy.component';
+import { LazyService } from './lazy.service';
 
 const routes: Routes = [{ path: '', component: LazyComponent }];
 
 @NgModule({
   declarations: [LazyComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
+  providers: [LazyService],
 })
 export class LazyModule {}
