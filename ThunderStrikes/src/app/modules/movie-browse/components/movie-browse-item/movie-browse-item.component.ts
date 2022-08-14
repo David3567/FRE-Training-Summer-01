adb8979af532901ac1dc6e804b59bf49d932be82
Phantom-Shen <whateverjackson@gmail.com>
2022-08-14 14:08:18 -0400

    route for movie browse

---
+++ ThunderStrikes/src/app/modules/movie-browse/components/movie-browse-item/movie-browse-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-browse-item',
+  templateUrl: './movie-browse-item.component.html',
+  styleUrls: ['./movie-browse-item.component.scss']
+})
+export class MovieBrowseItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ThunderStrikes/src/app/modules/movie-browse/components/movie-browse/movie-browse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-browse',
+  templateUrl: './movie-browse.component.html',
+  styleUrls: ['./movie-browse.component.scss']
+})
+export class MovieBrowseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
