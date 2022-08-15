2120bd01df1e4ea7f510895800c4bc54c357db86
Osamah <oneamah@gmail.com>
2022-08-15 18:38:21 -0400

    Auth Guard For Movielist P:age

---
+++ roe-movie-app/src/app/components/core/movielist/authentication.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+  constructor(private router:Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(localStorage.getItem('user')!=undefined) {return true}
+    else {this.router.navigate(['signin']); return false}
+  }
+
+}
