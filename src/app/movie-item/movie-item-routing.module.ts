21fd71f7768d437d1fcd203ec846813a6b0dbc12
Abel A <abelabrahamx@gmail.com>
2022-08-19 00:47:40 -0500

    movie item resolver add

--- src/app/movie-item/movie-item-routing.module.ts
+++ src/app/movie-item/movie-item-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieItemResolverService } from './movie-item-resolver.service';
 import { MovieItemComponent } from './movie-item/movie-item.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MovieItemComponent,
+    resolve: { keys: MovieItemResolverService },
   },
 ];
 
