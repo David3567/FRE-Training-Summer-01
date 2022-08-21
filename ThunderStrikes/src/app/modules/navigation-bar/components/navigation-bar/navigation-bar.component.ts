5e717f44c9266c83c1e56bbc48673b0a120b79d2
victorincode <promos.ripples0o@icloud.com>
2022-08-21 18:32:44 -0400

    There’s a bug where the navigation bar UI will not update when you sign in until you refresh the page. Tried to troubleshoot a bit but it’s been annoying me. Will try to figure it out tomorrow.

--- ThunderStrikes/src/app/modules/navigation-bar/components/navigation-bar/navigation-bar.component.ts
+++ ThunderStrikes/src/app/modules/navigation-bar/components/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.services';
 import { RoutingPages } from '../../../../shared/interfaces/routing-pages.interface';
 @Component({
     selector: 'app-navigation-bar',
@@ -9,11 +10,18 @@ import { RoutingPages } from '../../../../shared/interfaces/routing-pages.interf
 export class NavigationBarComponent implements OnInit {
     routingPages: typeof RoutingPages;
     readonly restrictedUrls: string[] = ['/', '/login', '/register'];
-    constructor(readonly router: Router) {
+    constructor(private readonly router: Router, private readonly authService: AuthService) {
         this.routingPages = RoutingPages;
     }
     get isRestrictedUrl() {
         return this.restrictedUrls.includes(this.router.url) ? true : false;
+    }
+    get isLoggedIn(): boolean{
+        return this.authService.isLoggedIn;
+    }
+
+    signOut(){
+        this.authService.signOut();
     }
     // will add routing to all below after merge, also need to pass account ID: number as each page will differ based on ID
     toSearch(): void {
--- ThunderStrikes/src/app/shared/services/auth.services.ts
+++ ThunderStrikes/src/app/shared/services/auth.services.ts
@@ -1,57 +1,110 @@
-import { Inject, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import jwt_decode from 'jwt-decode';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { User } from "../interfaces/user.interface";
 import { of } from 'rxjs';
+import { LocalStorageVariables } from "../interfaces/local-storage-variables";
+import jwtDecode from "jwt-decode";
+import { Router } from "@angular/router";
 
 
-@Injectable()
+@Injectable(
+  { providedIn: "root" }
+)
 export class AuthService {
-    private userAuthInfo: User = {};
-    private userAuth$ = new BehaviorSubject<User>(this.userAuthInfo);
-    private baseUrl = "http://localhost:4231";
-    userAuthObs$ = this.userAuth$.asObservable();
-  
-    get userInfo() {
-      return this.userAuth$.value;
-    }
-  
-    constructor(
-      private readonly http: HttpClient,
-    ) {}
-  
-    signin(loginInfo: User) {
-      return this.http.post<{ accessToken: string }>(
-        `${this.baseUrl}/auth/signin`,
-        loginInfo
-      ).pipe(
-        tap(({accessToken}) => {
-          const {username, id, email, role, tmdb_key, exp}: User = jwt_decode(accessToken);
-          this.userAuthInfo = {
-            username, 
-            id, 
-            email, 
-            role, 
-            tmdb_key, 
-            exp, 
-            jwt_token: accessToken
-          };
-          this.userAuth$.next(this.userAuthInfo)
-        }),
-        catchError(err => {
-          console.log(err);
-          return of('cannot find this user')
-        })
-      )
-    }
+  private _userAuthInfo: User = {};
+  private _isLoggedIn: boolean = false;
+  private userAuth$ = new BehaviorSubject<User>(this.userAuthInfo);
+  private baseUrl = "http://localhost:4231";
+  userAuthObs$ = this.userAuth$.asObservable();
 
-    register(registerInfo: User){
-        const url = `${this.baseUrl}/auth/signup`;
+  constructor(
+    private readonly http: HttpClient,
+    private readonly router: Router
+  ) { }
+  get userInfo() {
+    return this.userAuth$.value;
+  }
+  set userAuthInfo(newInfo: User) {
+    this._userAuthInfo = newInfo;
+    this.userAuth$.next(this._userAuthInfo);
+  }
+  get userAuthInfo() {
+    return this._userAuthInfo;
+  }
+  set isLoggedIn(value: boolean){
+    this._isLoggedIn = value;
+  }
+  get isLoggedIn(): boolean {
+    return this._isLoggedIn;
+  }
+  signin(loginInfo: User) {
+    return this.http.post<{ accessToken: string }>(
+      `${this.baseUrl}/auth/signin`,
+      loginInfo
+    ).pipe(
+      tap(({ accessToken }) => {
+        const userInfo: User = jwt_decode(accessToken);
+        this.userAuthInfo = userInfo;
+        this.setToken(accessToken);
+        console.log("Signed in.");
+        this.isLoggedIn = true;
+        console.log("this.isLoggedIn: ", this.isLoggedIn);
+      }),
+      catchError(err => {
+        console.log(err);
+        return of('cannot find this user')
+      })
+    )
+  }
+  signOut() {
+    localStorage.removeItem(LocalStorageVariables.JWT_TOKEN);
+    this.userAuthInfo = {};
+    this.isLoggedIn = false;
+    this.router.navigate([""]);
+    console.log("You have signed out.");
+  }
+
+  register(registerInfo: User) {
+    const url = `${this.baseUrl}/auth/signup`;
     return this.http.post<{ accessToken: string }>(
       url,
       registerInfo
     )
+  }
+
+  refreshToken() {
+    // No need to refresh the token if they've never logged in.
+    if (!localStorage.getItem(LocalStorageVariables.JWT_TOKEN)) return;
+    return this.http.post<{ accessToken: string }>([this.baseUrl, "auth", "refresh-token"].join("/"), this.userAuthInfo).pipe(
+      catchError(_ => {
+        console.error("User credentials are no longer valid.");
+        this.signOut();
+        return of();
+      }),
+      tap(token => {
+        this.setToken(token.accessToken);
+      })
+    );
+  }
+
+
+  private setToken(newToken: string) {
+    localStorage.setItem(LocalStorageVariables.JWT_TOKEN, newToken);
+    console.log("Successfully updated token.");
+  }
+  private getUserToken() {
+    return localStorage.getItem(LocalStorageVariables.JWT_TOKEN);
+  }
+  setUserInfo() {
+    const userToken = this.getUserToken();
+    if (userToken) {
+      const userData: User = jwtDecode(userToken);
+      this.userAuthInfo = userData;
+      console.log("Welcome back,", this.userAuthInfo.username);
+      this.isLoggedIn = true;
     }
   }
+}
