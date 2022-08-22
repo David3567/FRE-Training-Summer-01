791c27110ec2a347559dbc09288e4bef02dd71a6
Johnathan <jonathanumana86@gmail.com>
2022-08-22 16:33:17 -0400

    Fixed merge conflicts

--- Movie-App/src/app/services/movie.service.ts
+++ Movie-App/src/app/services/movie.service.ts
@@ -12,6 +12,8 @@ export class MovieService {
   RAW_URL: string = `https://api.themoviedb.org/3/discover/movie?api_key=3b12cfa2e8e41ce85be82944f8b7e697&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
   PagesUrl: string = `https://api.themoviedb.org/3/discover/movie?api_key=3b12cfa2e8e41ce85be82944f8b7e697`;
   page: number = 0
+  MOVIE_URL: string = `https://api.themoviedb.org/3/movie/157336?api_key=3b12cfa2e8e41ce85be82944f8b7e697&append_to_response=videos`
+  api_key: string = `3b12cfa2e8e41ce85be82944f8b7e697`
   constructor(private http: HttpClient) {}
 
   //Get Movies
@@ -25,5 +27,10 @@ export class MovieService {
     return this.http.get<Movie>(this.PagesUrl , {
       params: { page: this.page }
     })
+  }
+
+  getMovies(): Observable<MovieDiscoverList> {
+    return this.http.get<MovieDiscoverList>(this.MOVIE_URL
+    );
   }
 }
